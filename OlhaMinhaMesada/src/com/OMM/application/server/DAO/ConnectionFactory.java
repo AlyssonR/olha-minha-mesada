ca22d79003ee0b2b1a0a78c6bd37369df5e2474c
Tomaz Martins <tomaz.unb@outlook.com>
2013-10-07 21:40:25 -0300

    Implementação da ConnectionFactory

--- OlhaMinhaMesada/src/com/OMM/application/server/DAO/ConnectionFactory.java
+++ OlhaMinhaMesada/src/com/OMM/application/server/DAO/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package com.OMM.application.server.DAO;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+	
+	private final static String DATABASE = "";
+	private final static String USERNAME = "";
+	private final static String PASSWORD = "";
+	
+	public Connection getConnection() {
+		
+		try {
+			return DriverManager.getConnection(DATABASE, USERNAME, PASSWORD);
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
